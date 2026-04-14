3e667da06386f73c5cfc659da6b116e225f79490
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-14 22:41:47 +0600

    Add files via upload

---
+++ D_Fixed_Password.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+
+    //D. Fixed Password
+   int n;
+   while(scanf("%d",&n)){
+
+if(n==1999){
+    printf("Correct\n");
+    break;
+}
+else{
+    printf("Wrong\n");
+}
+
+
+
+   }
+
+
+return 0;
+}
