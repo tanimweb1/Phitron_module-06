3edc32091ab17e0855e5f34c8c6569905c31b325
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-14 22:41:29 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -59,3 +59,34 @@ int main()
 
     return 0;
 }
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    //D. Fixed Password
+   int n;
+   while(scanf("%d",&n)){
+
+if(n==1999){
+    printf("Correct\n");
+    break;
+}
+else{
+    printf("Wrong\n");
+}
+
+
+
+   }
+
+
+return 0;
+}
+
+
+
