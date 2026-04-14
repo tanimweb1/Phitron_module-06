870d9de232e4b898ce64d180ec6bc9186fd80d95
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-14 22:32:04 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -14,3 +14,47 @@ for(int i = 1; i<=n;i++){
 
 return 0;
 }
+
+
+
+
+
+#include <stdio.h>
+int main()
+{
+
+//C. Even, Odd, Positive and Negative
+    
+    int n;
+    scanf("%d", &n);
+    int even = 0, odd = 0, pos = 0, neg = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int a;
+        scanf("%d", &a);
+
+        if (a % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+        if (a >= 1)
+        {
+            pos++;
+        }
+        else if (a < 0)
+        {
+            neg++;
+        }
+    }
+
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Positive: %d\n", pos);
+    printf("Negative: %d\n", neg);
+
+    return 0;
+}
