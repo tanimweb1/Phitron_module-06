0d7bc54a5bd83ba92b8a364e75d9720d4a661499
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-14 22:44:53 +0600

    Add files via upload

---
+++ F_Multiplication_table.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+//Multiplication Table
+
+    int n;
+    scanf("%d",&n);
+
+    for(int i = 1;i<=12;i++){
+printf("%d * %d = %d\n",n,i,n*i);
+
+    }
+
+return 0;
+}
