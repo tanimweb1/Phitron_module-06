f55b6a6ec05ddff03213bfa544120c2f8dc1ec31
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-14 22:45:14 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -90,3 +90,27 @@ return 0;
 
 
 
+
+
+
+
+#include<stdio.h>
+int main(){
+
+//Multiplication Table
+
+    int n;
+    scanf("%d",&n);
+
+    for(int i = 1;i<=12;i++){
+printf("%d * %d = %d\n",n,i,n*i);
+
+    }
+
+return 0;
+}
+
+
+
+
+
