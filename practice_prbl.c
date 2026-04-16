45c408c86114ebc45c42036d2a4f3cf77f7a40dc
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-16 19:55:49 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -227,3 +227,49 @@ printf("%c",ch+1);
   
 return 0;
 }
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    //D. Ali Baba and Puzzles
+   long long int a,b,c;
+    long long int d;
+    scanf("%lld%lld%lld%lld",&a,&b,&c,&d);
+if(a+b-c==d){
+    printf("YES\n");
+}
+else if(a+b*c==d){
+    printf("YES\n");
+}
+else if(a-b+c==d){
+    printf("YES\n");
+}
+
+else if(a-b*c==d){
+    printf("YES\n");
+}
+else if(a*b-c==d){
+    printf("YES\n");
+}
+
+else if(a*b+c==d){
+    printf("YES\n");
+}
+
+else{
+    printf("NO\n");
+}
+
+
+
+
+return 0;
+}
+
+
+
