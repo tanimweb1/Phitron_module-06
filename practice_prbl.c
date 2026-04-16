5e99cc65acf76d3e98a7ad4e05022ebcb3e41f6a
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-16 18:58:03 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -144,5 +144,41 @@ return 0;
 
 
 
+#include<stdio.h>
+int main(){
+
+//S. Sum of Consecutive Odd Numbers
+
+    int t;
+    scanf("%d",&t);
+
+    for(int i = 1;i<=t;i++){
+int a,b,max,min,sum = 0;
+scanf("%d%d",&a,&b);
+if(a>b){
+    max = a;
+    min = b;
+}
+else{
+    max = b;
+    min = a;
+}
+
+for(int i = min+1; i<max;i++){
+
+if(i%2!=0){
+    sum = sum + i;
+}
+
+}
+printf("%d\n",sum);
+
+
+
+    }
+    
+return 0;
+}
+
 
 
