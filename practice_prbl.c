da1212a8ce64e7b05983ccde743d551f6500969b
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-16 19:10:02 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -182,3 +182,23 @@ return 0;
 
 
 
+
+
+#include<stdio.h>
+int main(){
+
+    //K. Divisors
+    int n;
+    scanf("%d",&n);
+
+    for (int i = 1; i<=n;i++){
+
+        if(n%i==0){
+            printf("%d\n",i);
+        }
+
+
+    }
+    
+return 0;
+}
