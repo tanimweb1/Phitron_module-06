dcd2b6f3b4cf1236fedb9658417c87dddca31814
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-16 19:35:06 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -202,3 +202,28 @@ int main(){
     
 return 0;
 }
+
+
+
+
+
+#include<stdio.h>
+int main(){
+
+    //C. Next Alphabet
+    char ch;
+    scanf("%c",&ch);
+
+if(ch=='z'){
+    printf("a");
+}
+
+else if(ch>='a' && ch<='z')
+    {
+printf("%c",ch+1);
+    }
+     
+    
+  
+return 0;
+}
