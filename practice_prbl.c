3cecd3a158eea8bec1861da3b3b5687feedd866a
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-04-16 21:39:58 +0600

    Update practice_prbl.c

--- practice_prbl.c
+++ practice_prbl.c
@@ -273,3 +273,27 @@ return 0;
 
 
 
+
+
+#include<stdio.h>
+int main(){
+
+    //I. Lucky Numbers
+
+    long long n,fast,last;
+    scanf("%lld",&n);
+
+    last = n%10;
+    fast = n/10;
+if (last == 0) {
+        printf("YES\n");
+}
+   else if(fast%last==0 || last%fast==0){
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+
+return 0;
+}
